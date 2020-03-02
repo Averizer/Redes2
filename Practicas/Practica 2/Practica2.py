48d6819e1ccf7e68373d91ab92a4ef481cdf93a1
WilverC <jcortezen09@hotmail.com>
2020-03-01 22:28:09 -0600

    cerrecion 2 zonas criticas

--- Practicas/Practica 2/Practica2.py
+++ Practicas/Practica 2/Practica2.py
@@ -7,28 +7,31 @@ import time
 #Constantes
 productores = 5
 consumidores = 5
-Nproducciones = 5
+Nproducciones = 2
 TamBuffer = 4
 tipoLetra = ['A', 'B', 'C', 'D', 'E']
 tipoNumero = ['1', '2', '3', '4', '5']
 
 #Zona critica
 zcLetras = ["","","",""]
-nzcNumeros = ['\0','\0','\0','\0']
-archivos = [None] * 5
+zcNumeros = ["","","",""]
+archivos = [None] * (len(tipoLetra)+len(tipoNumero))
 
 #declaracion de semaforos
 semGlobal = th.Semaphore(1)
 semArchivo = th.Semaphore(1)
-semZonaCritica = [None] * TamBuffer
+semZonaCriticaL = [None] * TamBuffer
+semZonaCriticaN = [None] * TamBuffer
 
 #creacion de los semaforos del buffer
 for i in range(0,TamBuffer):
-    semZonaCritica[i] = th.Semaphore()
+    semZonaCriticaL[i] = th.Semaphore()
+    semZonaCriticaN[i] = th.Semaphore()
 
-"""for f in range(0,len(tipoLetra)):
-    archivos[f] = open("Archivo_" + str(tipoLetra[f]) + ".txt","w+" )"""
-
+#creacion de nombres de archivos
+for f in range(0,len(tipoLetra)):
+    archivos[f] = "Archivo_" + str(tipoLetra[f]) + ".txt"
+    archivos[f+len(tipoLetra)] = "Archivo_" + str(tipoNumero[f]) + ".txt"
 
 
 # Funciones para los hilos
@@ -37,42 +40,91 @@ def productor(id):
     numero = tipoNumero[id]
     print("Hola soy el hilo productor ", id)
     aux = 0
-    for i in range(5):
+    for i in range(Nproducciones*2):
         while True:
-            #print("iterando en productor: "+ str(i))
+            #reseteo de indice
             if(aux == 3):
                 aux = 0
-            
-            semZonaCritica[aux].acquire()
-            if(len(zcLetras[aux]) is 0):
-                zcLetras[aux] = letra
-                semZonaCritica[aux].release()
-                print(zcLetras)
-                break
-            else:
-                semZonaCritica[aux].release()
-                time.sleep(0.2)
-                aux += 1
+            #Determina numero o letra
+            if(i%2 is 0): #Numeros
+                simbolo = numero
+                semZonaCriticaN[aux].acquire()
+                if(len(zcNumeros[aux]) is 0):
+                    zcNumeros[aux] = simbolo
+                    semZonaCriticaN[aux].release()
+                    print(zcNumeros)
+                    break
+                else:
+                    semZonaCriticaN[aux].release()
+                    time.sleep(0.2)
+                    aux += 1
+            else: #Letras
+                simbolo = letra
+                semZonaCriticaL[aux].acquire()
+                if(len(zcLetras[aux]) is 0):
+                    zcLetras[aux] = simbolo
+                    semZonaCriticaL[aux].release()
+                    print(zcLetras)
+                    break
+                else:
+                    semZonaCriticaL[aux].release()
+                    time.sleep(0.2)
+                    aux += 1
 
 def consumidor(id):
-    for i in range(5):
-        #print("Iteración numero: "+ str(i))
-        aux = 0
+    aux = 0
+    for i in range(Nproducciones*2):
         while True:
+            #reseteo de indice
             if(aux == 3):
                 aux = 0
+            #consumir dependiendo de paridad
+            if(i%2 is 0): #numeros
+                semZonaCriticaN[aux].acquire()
+                if (len(zcNumeros[aux]) is not 0):
+                    letraR = zcNumeros[aux]
+                    #escribiendo
+                    #semArchivo.acquire()
+                    if(letraR == tipoLetra[id]):
+                        print(archivos[id])
+                        """open(archivos[id])
+                        archivos[id].write(letraR+"\n")
+                        semArchivo.release()
+                    if(letraR == tipoNumero[id]):
+                        archivos[id+5].write(letraR+"\n")
+                        semArchivo.release()"""
 
-            semZonaCritica[aux].acquire()
-            if (len(zcLetras[aux]) is not 0):
-                letraR = zcLetras[aux]
-                print("Consumidor "+str(id)+" consumiendo: ", letraR)
-                zcLetras[aux] = ""
-                semZonaCritica[aux].release()
-                break
-            else:
-                semZonaCritica[aux].release()
-                time.sleep(0.2)
-                aux += 1
+                    print("Consumidor "+str(id)+" consumiendo: ", letraR)
+                    zcNumeros[aux] = ""
+                    semZonaCriticaN[aux].release()
+                    break
+                else:
+                    semZonaCriticaN[aux].release()
+                    time.sleep(0.2)
+                    aux += 1
+            else: #letras
+                semZonaCriticaL[aux].acquire()
+                if (len(zcLetras[aux]) is not 0):
+                    letraR = zcLetras[aux]
+                    #escribiendo
+                    #semArchivo.acquire()
+                    if(letraR == tipoLetra[id]):
+                        print(archivos[id])
+                        """open(archivos[id])
+                        archivos[id].write(letraR+"\n")
+                        semArchivo.release()
+                    if(letraR == tipoNumero[id]):
+                        archivos[id+5].write(letraR+"\n")
+                        semArchivo.release()"""
+
+                    print("Consumidor "+str(id)+" consumiendo: ", letraR)
+                    zcLetras[aux] = ""
+                    semZonaCriticaL[aux].release()
+                    break
+                else:
+                    semZonaCriticaL[aux].release()
+                    time.sleep(0.2)
+                    aux += 1
     
     print("Consumidor "+str(id)+" termino de consumir")
 
@@ -99,12 +151,8 @@ for i in range(5):
     p.join()
     p2 = hilosC[i]
     p2.join()
-    
-#cierre de los archivos
-"""for f in range(0,len(tipoLetra)):
-    archivos[f].close()"""
+
+
 
 print("Terminaron todos los hilos bye")
 
-
-ssiiiiiii ya esta xD 
