890c7ab845a5f8383ba3aae557fbcf80ad82242c
Averizer <36316073+Averizer@users.noreply.github.com>
2020-05-26 12:13:33 -0500

    dfsd
    fsdfs

--- Practicas/PracticaBRC/Cliente.py
+++ Practicas/PracticaBRC/Cliente.py
@@ -3,8 +3,8 @@ import socket
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) # UDP
 
 # Enable port reusage so we will be able to run multiple clients and servers on single (host, port). 
-# Do not use socket.SO_REUSEADDR except you using linux(kernel<3.9): goto https://stackoverflow.com/questions/14388706/how-do-so-reuseaddr-and-so-reuseport-differ for more information.
-# For linux hosts all sockets that want to share the same address and port combination must belong to processes that share the same effective user ID!
+# For linux hosts all sockets that want to share the same address and port combination must belong to processes 
+# that share the same effective user ID!
 # So, on linux(kernel>=3.9) you have to run multiple servers and clients under one user to share the same (host, port).
 # Thanks to @stevenreddie
 client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
@@ -13,7 +13,34 @@ client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
 client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
 client.bind(("", 37020))
+
 while True:
-    # Thanks @seym45 for a fix
-    data, addr = client.recvfrom(1024)
-    print("received message: %s"%data)
+    for x in range(5):
+        y = x + 1
+        fname2 = "" + str(y) + ".jpg"
+        fname = "/home/emiliano/Escritorio/Redes/BRC/img/" + fname2
+        img = open("/home/emiliano/Escritorio/Redes/BRC/imagen", "wb")
+
+        try:
+            data = client.recv(1024)
+        except socket.error:
+            print("Error de lectura.")
+            break
+        else:
+            if data:
+                if isinstance(data, bytes):
+                    end = data[0] == 1
+                else: 
+                    end = data == chr(1)
+                if not end:
+                    img.write(data)
+                else:
+                    img.close()
+                    break
+        print("Image recived successfully ")
+        
+    #client.close()
+
+    #exit()
+    #data, addr = client.recvfrom(1024)
+    #print("received message: %s"%data)
---
+++ Practicas/PracticaBRC/broadcast_server.py
@@ -0,0 +1,30 @@
+import socket
+import time
+
+imagenes = ["1.png","2.png","3.png","4.png"]	#Lista con los nombres de las 4 imagenes a enviar
+
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)	#Se crea el socket UDP
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)					#Se configura para reutilizar
+server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)					#Se configura como broadcast
+
+while True:
+	for img in imagenes:										#for que recorre las cuatro imagenes
+		print("Transmitiendo imagen " + img)					#Imprimo el nombre del archivo que se esta transmitiendo
+		f = open(img, "rb")										#Abro el archibo en lectura de bytes
+		
+		content = f.read(512)									#Leo 512 bytes
+		content = bytes("1", "utf-8") + content					#Le concateno un 1 para que el cliente sepa que es el primer datagrama del archivo
+		while content:									
+			server.sendto(content, ('<broadcast>', 37020))		#Envio el datagrama
+			content = f.read(512)								#Leo 512 bytes
+			if len(content) < 512:								#Si leo menos de 512 esque es el ultimo datagrama
+				content = bytes("0", "utf-8") + content			#Le concateno un 0 para que el cliente sepa que es el ultimo
+				server.sendto(content, ('<broadcast>', 37020))	#Le envio el datagrama
+				time.sleep(2)									#Espero 2 segundos
+				break
+			else:												#En caso de que sea un datagrama intermedio
+				content = bytes("2", "utf-8") + content			#Le concateno un 2 para que el cliente sepa que es un datagrama intermedio
+
+		f.close()												#Cierro el archivo
+
+server.close()	#Cierro el socket 
